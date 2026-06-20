30860ef26e29d7bd1b9785eb6827dbec823c5c7b
Edem <155203167+2spi93@users.noreply.github.com>
2026-06-20 21:42:51 +0200

    Route cognitive genome through canonical residual topology

---
+++ src/hex_cortex/memory/cortex_cognitive_genome_v2.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from hex_cortex.memory.cortex_cognitive_genome import *  # noqa: F403
+from hex_cortex.memory.cortex_cognitive_residuals import append_cognitive_residual
+from hex_cortex.memory.cortex_cognitive_residuals import build_residual_signature
+from hex_cortex.memory.cortex_cognitive_residuals import project_residual_topology
+from hex_cortex.memory.cortex_cognitive_residuals import read_cognitive_records
+
+__all__ = [
+    "append_cognitive_residual",
+    "build_residual_signature",
+    "project_residual_topology",
+    "read_cognitive_records",
+]
